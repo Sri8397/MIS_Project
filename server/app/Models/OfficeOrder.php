02a8fa8f70dfa0ff9f891ad1f37681770bce98c4
Sri8397 <srikant9004@gmail.com>
2024-04-20 00:25:56 +0530

    Update project with new Notice model, notices table, and related changes in controllers

--- server/app/Models/OfficeOrder.php
+++ server/app/Models/OfficeOrder.php
@@ -1,22 +1,32 @@
 <?php
+
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OfficeOrder extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'title_en',
         'title_hi',
         'last_date_time',
-        'attachment',
-        'attachment_link',
         'remarks',
         'department_section_id',
+        'attachment',
+        'attachment_link',
+    ];
+
+    protected $dates = [
+        'last_date_time',
+        'created_at',
+        'updated_at',
     ];
 
     public function departmentSection()
     {
-        return $this->belongsTo(DepartmentSection::class, 'department_section_id');
+        return $this->belongsTo(DepartmentSection::class);
     }
 }
