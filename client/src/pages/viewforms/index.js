1d136295cee80ea22db59c864e19745143944ef6
upasanatharu <worship9771@gmail.com>
2024-04-20 11:05:12 +0530

    .

--- client/src/pages/viewforms/index.js
+++ client/src/pages/viewforms/index.js
@@ -1,22 +1,18 @@
 import React, { useState } from 'react';
-import { Button, Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-
+import { Button, Grid } from '@mui/material';
 import Component1 from './components/noticeTable';
 import Component2 from './components/officeOrderTable';
 import Component3 from './components/TenderTable';
 
-const useStyles = makeStyles((theme) => ({
-  grid: {
-    // backgroundColor:"black"
-  },
-  griditem: {
-    flex: "1"
-  }
-}));
+const Default = () => {
+  return (
+    <div style={{height:"94%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+      <div>Please select One of the options</div>
+    </div >
+  )
+}
 
 const MyComponent = () => {
-  const classes = useStyles();
 
   const [selectedOption, setSelectedOption] = useState(null);
 
@@ -33,39 +29,42 @@ const MyComponent = () => {
       case 'option3':
         return <Component3 />;
       default:
-        return null;
+        return <Default />;
     }
   };
 
   return (
-    <div>
-      <Grid className={classes.grid} container spacing={0} justify="center">
-        <Grid className={classes.griditem} item>
+    <div style={{height: "100%"}}>
+      <Grid container spacing={0} justify="center" style={{ display: "flex", justifyContent: 'center' }}>
+        <Grid style={{ flex: "1", padding: "5px" }} item>
           <Button
             variant="contained"
             fullWidth
             color={selectedOption === 'option1' ? 'secondary' : 'primary'}
             onClick={() => handleClick('option1')}
+            style={{ borderRadius: "5px" }}
           >
             Notices
           </Button>
         </Grid>
-        <Grid className={classes.griditem} item>
+        <Grid style={{ flex: "1", padding: "5px" }} item>
           <Button
             fullWidth
             variant="contained"
             color={selectedOption === 'option2' ? 'secondary' : 'primary'}
             onClick={() => handleClick('option2')}
+            style={{ borderRadius: "5px" }}
           >
             Office Orders
           </Button>
         </Grid>
-        <Grid className={classes.griditem} item>
+        <Grid style={{ flex: "1", padding: "5px" }} item>
           <Button
             fullWidth
             variant="contained"
             color={selectedOption === 'option3' ? 'secondary' : 'primary'}
             onClick={() => handleClick('option3')}
+            style={{ borderRadius: "5px" }}
           >
             Tenders
           </Button>
